1ef0874a70a9ba03776dcb81d6dfe0227658c449
blacklotus242 <eric.blacklotus@gmail.com>
2025-04-26 11:59:45 -0400

    Copy src/api/gmailApi.js from ai-assistant-project

---
+++ src/api/gmailApi.js
@@ -0,0 +1,6 @@
+export async function sendGmailEmail(to, subject, body) {
+  console.log('Simulating sending Gmail email to:', to);
+  console.log('Subject:', subject);
+  console.log('Body:', body);
+  // In real world: Use Gmail API with OAuth2 or App Password
+}
